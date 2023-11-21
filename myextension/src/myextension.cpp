8ca6e2ef9eac665ebbdd412d8256bd4e4421d2af
JustAPotota <justapotota.dev@gmail.com>
2023-11-21 14:38:31 -0600

    Open/close zip files

--- myextension/src/myextension.cpp
+++ myextension/src/myextension.cpp
@@ -1,106 +1,127 @@
 // myextension.cpp
 // Extension lib defines
-#define LIB_NAME "MyExtension"
-#define MODULE_NAME "myextension"
+#define LIB_NAME "Zip"
+#define MODULE_NAME "zip"
 
 // include the Defold SDK
 #include <dmsdk/sdk.h>
+#include <zip.h>
 
-static int Reverse(lua_State* L)
+struct ZipFile
 {
-    // The number of expected items to be on the Lua stack
-    // once this struct goes out of scope
+    dmZip::HZip* zip;
+    bool open;
+};
+
+static int ZipOpen(lua_State* L)
+{
     DM_LUA_STACK_CHECK(L, 1);
 
-    // Check and get parameter string from stack
-    char* str = (char*)luaL_checkstring(L, 1);
+    const char* path = (char*)luaL_checkstring(L, 1);
 
-    // Reverse the string
-    int len = strlen(str);
-    for(int i = 0; i < len / 2; i++) {
-        const char a = str[i];
-        const char b = str[len - i - 1];
-        str[i] = b;
-        str[len - i - 1] = a;
+    ZipFile* zipfile = (ZipFile*)lua_newuserdata(L, sizeof(ZipFile));
+
+    zipfile->zip = (dmZip::HZip*)malloc(sizeof(dmZip::HZip));
+
+    dmZip::Result result = dmZip::Open(path, zipfile->zip);
+    if (result != dmZip::RESULT_OK)
+        return luaL_error(L, "Zip file '%s' not found", path);
+
+    zipfile->open = true;
+
+    luaL_getmetatable(L, "zipfile");
+    lua_setmetatable(L, -2);
+
+    return 1;
+}
+
+static int ZipFileClose(lua_State* L)
+{
+    DM_LUA_STACK_CHECK(L, 0);
+
+    ZipFile* zipfile = (ZipFile*)lua_touserdata(L, 1);
+
+    if (zipfile->open)
+    {
+        dmZip::Close(*zipfile->zip);
+        zipfile->open = false;
     }
 
-    // Put the reverse string on the stack
-    lua_pushstring(L, str);
+    return 0;
+}
 
-    // Return 1 item
+static int ZipFileLength(lua_State* L)
+{
+    DM_LUA_STACK_CHECK(L, 1);
+
+    ZipFile* zipfile = (ZipFile*)lua_touserdata(L, 1);
+    uint32_t entry_count = dmZip::GetNumEntries(*zipfile->zip);
+
+    lua_pushnumber(L, entry_count);
+
     return 1;
 }
 
 // Functions exposed to Lua
-static const luaL_reg Module_methods[] =
+static const luaL_reg MODULE_METHODS[] =
 {
-    {"reverse", Reverse},
+    {"open", ZipOpen},
     {0, 0}
 };
+
+static const luaL_reg ZIPFILE_META[] =
+{
+    {"__len", ZipFileLength},
+    {"__gc", ZipFileClose},
+    {"close", ZipFileClose},
+    {0, 0}
+};
+
+// https://github.com/lua/lua/blob/6baee9ef9d5657ab582c8a4b9f885ec58ed502d0/lauxlib.c#L928
+void luaL53_setfuncs(lua_State *L, const luaL_Reg *l, int nup) {
+    luaL_checkstack(L, nup, "too many upvalues");
+    for (; l->name != NULL; l++) {  /* fill the table with given functions */
+        if (l->func == NULL)  /* place holder? */
+        lua_pushboolean(L, 0);
+        else {
+            int i;
+            for (i = 0; i < nup; i++)  /* copy upvalues to the top */
+            lua_pushvalue(L, -nup);
+            lua_pushcclosure(L, l->func, nup);  /* closure with those upvalues */
+        }
+        lua_setfield(L, -(nup + 2), l->name);
+    }
+    lua_pop(L, nup);  /* remove upvalues */
+}
+
+static void InitZipfileMetatable(lua_State* L)
+{
+    luaL_newmetatable(L, "zipfile");
+    lua_pushvalue(L, -1);
+    lua_setfield(L, -2, "__index");
+    luaL53_setfuncs(L, ZIPFILE_META, 0);
+    lua_pop(L, 1);
+}
 
 static void LuaInit(lua_State* L)
 {
     int top = lua_gettop(L);
 
     // Register lua names
-    luaL_register(L, MODULE_NAME, Module_methods);
+    luaL_register(L, MODULE_NAME, MODULE_METHODS);
+
+    InitZipfileMetatable(L);
 
     lua_pop(L, 1);
     assert(top == lua_gettop(L));
 }
 
-static dmExtension::Result AppInitializeMyExtension(dmExtension::AppParams* params)
-{
-    dmLogInfo("AppInitializeMyExtension");
-    return dmExtension::RESULT_OK;
-}
-
-static dmExtension::Result InitializeMyExtension(dmExtension::Params* params)
+static dmExtension::Result InitializeZipExtension(dmExtension::Params* params)
 {
     // Init Lua
     LuaInit(params->m_L);
     dmLogInfo("Registered %s Extension", MODULE_NAME);
     return dmExtension::RESULT_OK;
-}
-
-static dmExtension::Result AppFinalizeMyExtension(dmExtension::AppParams* params)
-{
-    dmLogInfo("AppFinalizeMyExtension");
-    return dmExtension::RESULT_OK;
-}
-
-static dmExtension::Result FinalizeMyExtension(dmExtension::Params* params)
-{
-    dmLogInfo("FinalizeMyExtension");
-    return dmExtension::RESULT_OK;
-}
-
-static dmExtension::Result OnUpdateMyExtension(dmExtension::Params* params)
-{
-    dmLogInfo("OnUpdateMyExtension");
-    return dmExtension::RESULT_OK;
-}
-
-static void OnEventMyExtension(dmExtension::Params* params, const dmExtension::Event* event)
-{
-    switch(event->m_Event)
-    {
-        case dmExtension::EVENT_ID_ACTIVATEAPP:
-            dmLogInfo("OnEventMyExtension - EVENT_ID_ACTIVATEAPP");
-            break;
-        case dmExtension::EVENT_ID_DEACTIVATEAPP:
-            dmLogInfo("OnEventMyExtension - EVENT_ID_DEACTIVATEAPP");
-            break;
-        case dmExtension::EVENT_ID_ICONIFYAPP:
-            dmLogInfo("OnEventMyExtension - EVENT_ID_ICONIFYAPP");
-            break;
-        case dmExtension::EVENT_ID_DEICONIFYAPP:
-            dmLogInfo("OnEventMyExtension - EVENT_ID_DEICONIFYAPP");
-            break;
-        default:
-            dmLogWarning("OnEventMyExtension - Unknown event id");
-            break;
-    }
 }
 
 // Defold SDK uses a macro for setting up extension entry points:
@@ -109,4 +130,4 @@ static void OnEventMyExtension(dmExtension::Params* params, const dmExtension::E
 
 // MyExtension is the C++ symbol that holds all relevant extension data.
 // It must match the name field in the `ext.manifest`
-DM_DECLARE_EXTENSION(MyExtension, LIB_NAME, AppInitializeMyExtension, AppFinalizeMyExtension, InitializeMyExtension, OnUpdateMyExtension, OnEventMyExtension, FinalizeMyExtension)
+DM_DECLARE_EXTENSION(Zip, LIB_NAME, 0, 0, InitializeZipExtension, 0, 0, 0)
